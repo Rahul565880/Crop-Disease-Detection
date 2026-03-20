8ad8aec3a3ac3f8a6feed2c22fee94c37a498d8b
Rahul Pawar <rp7772633@gmail.com>
2026-03-20 18:02:02 +0530

    Update CORS with Vercel frontend URL

--- server/app.js
+++ server/app.js
@@ -10,8 +10,20 @@ const adminRoutes = require('./routes/adminRoutes');
 
 const app = express();
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:5173',
+  'https://crop-disease-detection-chi.vercel.app'
+];
+
 app.use(cors({
-  origin: '*',
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
 app.use(express.json());
